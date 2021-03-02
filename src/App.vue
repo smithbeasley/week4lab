f0593eaf34a3cca63882798451cc3ef7c319ef61
smithbeasley@gmail.com <smithbeasley@gmail.com>
2021-03-02 00:35:27 -0600

    Lab4

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<h1 class="text-2xl font-bold">My To Do App</h1>
+<input type="text" placeholder="New Todo"/>
+<h3>Pending Items</h3>
+<ul>
+  <li>One</li>
+</ul>
+<h3>Completed Items</h3>
+<ul>
+  <li>Two</li>
+</ul>
+</template>
+
+
+<script setup></script>
+
